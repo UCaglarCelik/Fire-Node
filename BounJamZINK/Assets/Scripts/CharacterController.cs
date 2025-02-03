f4bb69e0f195773b987d3a499795ee59fe23fd1d
UCaglarCelik <umut_2955@outlook.com.tr>
2025-02-03 23:43:34 +0300

    loaded

---
+++ BounJamZINK/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public static CharacterController Instance { get; private set; }
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    
+    
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private Animator animator;
+    
+    public float moveSpeed = 5f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>(); // Animator bileşenini alın
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        bool isMoving = moveX != 0 || moveY != 0; // Hareket edip etmediğini kontrol edin
+        animator.SetBool("isMoving", isMoving); // Animator'daki parametreyi güncelleyin
+    
+        if (moveX == 1)
+        {
+            sr.flipX = false;
+        }
+        else if (moveX == -1)
+        {
+            sr.flipX = true;
+        }
+            
+        Vector2 movement = new Vector2(moveX, moveY).normalized;
+        rb.velocity = movement * moveSpeed;
+
+        if (movement == Vector2.zero)
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+}
